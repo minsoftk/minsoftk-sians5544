c505143492980c6b97c161d978288fce756946e7
Minsung Kim <minsoftk@gmail.com>
2021-12-20 17:55:29 +0900

    1번 리팩토링 완료

--- 1.Toggle Side Nav/js/app.js
+++ 1.Toggle Side Nav/js/app.js
@@ -1,20 +1,26 @@
 // DOM nodes
 const $iToggle = document.querySelector('.toggle');
 const $containerNav = document.querySelector('.container > nav');
+const $main = document.querySelector('.container > main');
 
-const test = () => {};
+const toggleTransition = isDisable => {
+  [$iToggle, $containerNav, $main].forEach(elem => {
+    elem.classList.toggle('notransition', isDisable);
+  });
+};
 
 const fetchToggleState = () => {
-  sessionStorage.getItem('isToggle') ? '' : sessionStorage.setItem('isToggle', false);
-  test();
+  let isToggle = JSON.parse(localStorage.getItem('isToggle'));
+  isToggle ? toggleTransition() : (isToggle = false);
+  $containerNav.classList.toggle('active', isToggle);
 };
 
 window.addEventListener('DOMContentLoaded', fetchToggleState);
 
-$iToggle.addEventListener('click', e => {
-  const isToggle = $containerNav.classList.contains('active');
-
-  sessionStorage.setItem('isToggle', isToggle);
-
+$iToggle.addEventListener('click', () => {
+  toggleTransition(false);
   $containerNav.classList.toggle('active');
+
+  const isToggle = $containerNav.classList.contains('active');
+  localStorage.setItem('isToggle', isToggle);
 });

d5b7342c173736a1474019fc6bd3d61a88e58744
Minsung Kim <minsoftk@gmail.com>
2021-12-20 22:45:47 +0900

    03 주석 추가

--- 03.Counter/app.js
+++ 03.Counter/app.js
@@ -21,3 +21,7 @@ $increaseBtn.addEventListener('click', () => {
 $decreaseBtn.addEventListener('click', () => {
   $counter.textContent = MakeCounter(decrease);
 });
+
+// decrease 함수에 n이 0일 경우 0을 return
+// innerHTML -> textContent로 수정 : 자식 요소가 없을 때 사용. 자식 요소의 모든 텍스트 노드 값을 가져오기 때문에.
+// closer 확실하게 이해하기

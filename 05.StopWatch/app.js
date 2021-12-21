ebe9a1b1ae10fdcaf85167e43a0cbcf61bf855f3
Minsung Kim <minsoftk@gmail.com>
2021-12-21 19:03:42 +0900

    05.StopWatch mk

--- 05.StopWatch/app.js
+++ 05.StopWatch/app.js
@@ -3,6 +3,7 @@ const $timerDisplay = document.querySelector('.stopwatch > .display');
 const $stopWatch = document.querySelector('.stopwatch');
 const $startBtn = $stopWatch.children[1];
 const $resetBtn = $stopWatch.children[2];
+const $laps = document.querySelectorAll('.laps');
 
 let miliSecond = 0;
 let second = 0;
@@ -17,12 +18,19 @@ let interval;
 // 2. Lap 누를 시 아래 눌렀을 때의 시간 기록 보여짐
 // 3. Reset 누르면 lap 날라가고 00:00:00 보여주기
 
+const printTimer = () => {
+  const sMinute = minute < 10 ? `0${minute}` : `${minute}`;
+  const sSecond = second < 10 ? `0${second}` : `${second}`;
+  const sMiliSecond = miliSecond < 10 ? `0${miliSecond}` : `${miliSecond}`;
+
+  $timerDisplay.textContent = `${sMinute}:${sSecond}:${sMiliSecond}`;
+};
+
 $startBtn.addEventListener('click', e => {
   e.target.textContent = isStart ? 'Stop' : 'Start';
   $resetBtn.textContent = isStart ? 'Lap' : 'Reset';
-
+  if ($resetBtn.hasAttribute('disabled')) $resetBtn.removeAttribute('disabled');
   if (isStart) {
-    $resetBtn.removeAttribute('disabled');
     interval = setInterval(() => {
       if (second === 60) {
         minute++;
@@ -33,12 +41,7 @@ $startBtn.addEventListener('click', e => {
         miliSecond = 0;
       }
 
-      const sMinute = minute < 10 ? `0${minute}` : `${minute}`;
-      const sSecond = second < 10 ? `0${second}` : `${second}`;
-      const sMiliSecond = miliSecond < 10 ? `0${miliSecond}` : `${miliSecond}`;
-
-      $timerDisplay.textContent = `${sMinute}:${sSecond}:${sMiliSecond}`;
-
+      printTimer();
       miliSecond++;
     }, 10);
   } else {
@@ -48,12 +51,28 @@ $startBtn.addEventListener('click', e => {
   isStart = !isStart;
 });
 
-$resetBtn.addEventListener('click', e => {
-  // if(isStart)
+$resetBtn.addEventListener('click', () => {
+  // stop 버튼일때, lap
+  // laps()
+
+  // reset()
+  if ($resetBtn.textContent === 'Reset' && !$resetBtn.getAttribute('disabled')) $resetBtn.setAttribute('disabled', '');
+
+  // laps일때
+  if (!isStart) {
+    $laps.style.display = 'grid';
+    $laps.innerHTML += `<div>${$laps}</div>`;
+  } else {
+    miliSecond = 0;
+    second = 0;
+    minute = 0;
+    printTimer();
+  }
 });
 
 // 재할당 or 밀리세커즈로 계산 어느 것이 더 효율적인가? 가독성이 좋은가?
 // 웹 페이지 동작 vs 웹 어플리케이션 상태 저장
+// disabled = true , disabled
 
 // $display.textContent = `${minute < 10 ? `0${minute}` : `${minute}`}:${second < 10 ? `0${second}` : `${second}`}:${
 // 	miliSecond < 10 ? `0${miliSecond}` : `${miliSecond}`

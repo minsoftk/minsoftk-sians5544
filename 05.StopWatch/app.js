ae70546f11c7492f726aca878f9c59c2eb42dced
Minsung Kim <minsoftk@gmail.com>
2021-12-22 00:46:37 +0900

    05. 주석 정리

--- 05.StopWatch/app.js
+++ 05.StopWatch/app.js
@@ -6,23 +6,17 @@ const $resetBtn = $stopWatch.children[2];
 const $laps = document.querySelector('.laps');
 
 let miliSecond = 0;
-const second = 0;
-const minute = 0;
+// const second = 0;
+// const minute = 0;
 
 let isStart = 1;
-// todo list
-
 let interval = 0;
 
-// 1.start  버튼 누를 시 stop 으로 변경  2번째 버튼의 Lap 으로 명칭 변경 , disabled 삭제
-// 2. Lap 누를 시 아래 눌렀을 때의 시간 기록 보여짐
-// 3. Reset 누르면 lap 날라가고 00:00:00 보여주기
-
+// 1
 // const printTimer = () => {
 //   const sMinute = minute < 10 ? `0${minute}` : `${minute}`;
 //   const sSecond = second < 10 ? `0${second}` : `${second}`;
 //   const sMiliSecond = miliSecond < 10 ? `0${miliSecond}` : `${miliSecond}`;
-
 //   return `${sMinute}:${sSecond}:${sMiliSecond}`;
 // };
 
@@ -36,6 +30,7 @@ $startBtn.addEventListener('click', e => {
     interval = setInterval(() => {
       miliSecond += 1;
 
+      // 2.
       const mm = Math.floor(miliSecond / 6000);
       const ss = Math.floor(miliSecond / 100) % 60;
       const ms = miliSecond % 100;
@@ -43,6 +38,7 @@ $startBtn.addEventListener('click', e => {
         ms < 10 ? `0${ms}` : `${ms}`
       }`;
 
+      // 2.
       // if (second === 60) {
       //   minute++;
       //   second = 0;
@@ -64,20 +60,18 @@ $startBtn.addEventListener('click', e => {
 
 let id = 1;
 $resetBtn.addEventListener('click', () => {
-  // stop 버튼일때, lap
-  // laps()
-
-  // reset()
   if ($resetBtn.textContent === 'Reset' && !$resetBtn.getAttribute('disabled')) $resetBtn.setAttribute('disabled', '');
 
-  // laps일때
   if (!isStart) {
     $laps.style.display = 'grid';
     $laps.innerHTML += `<div>${id++}</div><div>${$timerDisplay.textContent}</div>`;
   } else {
+    // 3.
     // miliSecond = 0;
     // second = 0;
     // minute = 0;
+
+    // 3.
     $timerDisplay.textContent = '00:00:00';
     // $timerDisplay.textContent = printTimer();
     $laps.innerHTML = '<div>Laps</div><div>Time</div>';

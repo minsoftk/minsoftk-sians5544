4c6ea1508d3b62154440d33ea019caada13e8706
Minsung Kim <minsoftk@gmail.com>
2021-12-22 22:37:15 +0900

    08.Tabs 요소 선택 완료

--- 08.Tabs/app.js
+++ 08.Tabs/app.js
@@ -1,7 +1,11 @@
 // fetch fake data
 // eslint-disable-next-line arrow-body-style
-const fetchTabsData = () => {
-  return new Promise(resolve => {
+
+const $tabs = document.querySelector('.tabs');
+
+let todos = [];
+const fetchTabsData = () =>
+  new Promise(resolve => {
     setTimeout(
       () =>
         resolve([
@@ -21,6 +25,50 @@ const fetchTabsData = () => {
       1000
     );
   });
+
+const render = () => {
+  const $nav = document.createElement('nav');
+  todos.forEach((todo, idx) => {
+    const $div = document.createElement('div');
+    $div.classList.add('tab');
+    $div.setAttribute('data-index', idx);
+    $div.textContent = todo.title;
+    $nav.appendChild($div);
+  });
+
+  const $span = document.createElement('span');
+  $span.classList.add('glider');
+  $nav.appendChild($span);
+  $tabs.appendChild($nav);
+
+  todos.forEach((todo, idx) => {
+    const $div = document.createElement('div');
+    idx ? $div.classList.add('tab-content') : $div.classList.add('tab-content', 'active');
+    // toggle로 바꿔보기
+    $div.textContent = todo.content;
+    $tabs.appendChild($div);
+  });
 };
 
-// Do something!
+window.addEventListener('DOMContentLoaded', () => {
+  fetchTabsData().then(response => {
+    todos = response;
+    render();
+  });
+});
+
+// tabs의 자식요소 .tab 에 이벤트 위임 , data-index를 저장
+// tab-content에서 active 어트리뷰트 찾아서 제거해주고, data-index번의 요소에 active를 추가.
+$tabs.addEventListener('click', e => {
+  const $tabContent = document.querySelectorAll('.tab-content');
+
+  [...$tabContent].forEach((content, index) => {
+    content.classList.toggle('active', index === +e.target.dataset.index);
+  });
+});
+
+// $tabs 이벤트 리스너에서 자식 요소의 $tabContent를 받아오지 못함.
+
+// todo
+// loading 없애는 것.
+// tab 클릭했을 때, 색깔 변경해주기

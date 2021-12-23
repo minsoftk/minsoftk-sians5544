59da0e2d2abe662843966b758831d8020b5fd060
Minsung Kim <minsoftk@gmail.com>
2021-12-24 00:48:31 +0900

    10.Toaster 기능 구현 완료

---
+++ 10.Toaster/app.js
@@ -0,0 +1,72 @@
+const $body = document.body;
+const $showSuccess = document.querySelector('.show-success');
+const $showError = document.querySelector('.show-error');
+const $showWarning = document.querySelector('.show-warning');
+
+const title = { success: 'Well done!', error: 'Check it Out!', warning: 'Check it Out!' };
+
+const toastBox = category => {
+  const $len = document.querySelectorAll('.toast');
+
+  const $div = document.createElement('div');
+  $div.className = `toast toast-${category}`;
+  document.body.appendChild($div);
+  $div.innerHTML = `
+	<h4 class="toast-heading">${title[category]} ${$len.length}</h4>
+		<div class="toast-message">
+			<svg width="24" height="24">
+				<use href="#${category}" />
+			</svg>
+			<p> This is a ${category} alert</p>
+		</div>
+		<a class="close">&times;</a>`;
+  // document.body.insertAdjacentHTML(
+  //   'beforeend',
+  //   `
+  // 	<h4 class="toast-heading">Title</h4>
+  // 		<div class="toast-message">
+  // 			<svg width="24" height="24">
+  // 				<use href="#success" />
+  // 			</svg>
+  // 			<p>Message</p>
+  // 		</div>
+  // 		<a class="close">&times;</a>`
+  // );
+  setTimeout(() => {
+    document.body.removeChild($div);
+  }, 10000000);
+};
+
+const adjustHeight = () => {
+  const $toasts = document.querySelectorAll('.toast');
+  $toasts.forEach((item, idx) => {
+    const cal = item.offsetHeight * ($toasts.length - 1 - idx);
+    if (idx !== $toasts.length - 1) item.style.bottom = `${cal}px`;
+  });
+  $toasts[$toasts.length - 1].style.bottom = '0';
+};
+
+$showSuccess.addEventListener('click', () => {
+  toastBox('success');
+  adjustHeight();
+});
+
+$showError.addEventListener('click', () => {
+  toastBox('error');
+  adjustHeight();
+});
+
+$showWarning.addEventListener('click', () => {
+  toastBox('warning');
+  adjustHeight();
+});
+
+$body.addEventListener('click', e => {
+  if (e.target.className !== 'close') return;
+  $body.removeChild(e.target.parentNode);
+  adjustHeight();
+});
+
+// SVG 2 removed the need for the xlink namespace, so instead of xlink:href you should use href.
+// success 이벤트가 한번밖에 발생 안함 원인 : toastBox('success'); => innerHTML을 사용해서
+// setTimeout 설정 문제
